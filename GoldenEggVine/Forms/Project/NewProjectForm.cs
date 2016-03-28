f3e47bd8f08167d7b6dc8fb1affd20c05323103b
Cedric Schneider <default3@web.de>
2016-03-28 23:39:10 +0200

    Ask user to clean selected project directory

--- GoldenEggVine/Forms/Project/NewProjectForm.cs
+++ GoldenEggVine/Forms/Project/NewProjectForm.cs
@@ -23,12 +23,11 @@ namespace GoldenEggVine.Forms.Project
 
 		private void browseBaseRomPathButton_Click(object sender, EventArgs e)
 		{
-			OpenFileDialog ofd = new OpenFileDialog()
-				{
-					Filter = "SNES ROMs (*.smc/*.sfc)|*.smc;*.sfc",
-					Title = "Select a headered Yoshi's Island (U) (1.0) ROM"
-				};
-			if (ofd.ShowDialog() == DialogResult.OK)
+			OpenFileDialog ofd = new OpenFileDialog() {
+				Filter = "SNES ROMs (*.smc/*.sfc)|*.smc;*.sfc",
+				Title = "Select a headered Yoshi's Island (U) (1.0) ROM"
+			};
+			if(ofd.ShowDialog() == DialogResult.OK)
 			{
 				baseRomPathTextBox.Text = ofd.FileName;
 			}
@@ -36,13 +35,28 @@ namespace GoldenEggVine.Forms.Project
 
 		private void browseProjectParentDirButton_Click(object sender, EventArgs e)
 		{
-			FolderBrowserDialog fbd = new FolderBrowserDialog()
-				{
-					ShowNewFolderButton = true
-				};
-			if (fbd.ShowDialog() == DialogResult.OK)
+			FolderBrowserDialog fbd = new FolderBrowserDialog() {
+				ShowNewFolderButton = true
+			};
+			if(fbd.ShowDialog() == DialogResult.OK)
 			{
 				projectParentDirTextBox.Text = fbd.SelectedPath;
+			}
+
+			if(Directory.GetFileSystemEntries(fbd.SelectedPath).Length > 0)
+			{
+				var result = MessageBox.Show("The directory you selected is not empty. You can choose to delete its contents and create the project or choose a different directory. Do you want to choose a different directory?", "WARNING!! DANGER!!!", MessageBoxButtons.YesNo);
+				if(result == DialogResult.Yes)
+				{
+					browseProjectParentDirButton_Click(sender, e);
+				}
+				else
+				{
+					foreach(var path in Directory.GetFiles(fbd.SelectedPath))
+						File.Delete(path);
+					foreach(var path in Directory.GetDirectories(fbd.SelectedPath))
+						Directory.Delete(path, true);
+				}
 			}
 		}
 
@@ -50,7 +64,7 @@ namespace GoldenEggVine.Forms.Project
 		{
 			createProjectButton.Enabled = false;
 			// Project can be created if the ROM file exists, it's a valid ROM, the directory exists and the project name can be converted into a folder name
-			if (File.Exists(baseRomPathTextBox.Text) && Directory.Exists(projectParentDirTextBox.Text) && projectNameTextBox.Text.IndexOfAny(Path.GetInvalidFileNameChars()) == -1)
+			if(File.Exists(baseRomPathTextBox.Text) && Directory.Exists(projectParentDirTextBox.Text) && projectNameTextBox.Text.IndexOfAny(Path.GetInvalidFileNameChars()) == -1)
 			{
 				try
 				{
